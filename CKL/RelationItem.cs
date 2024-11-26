51c880b447badbc0b97e9f89313fb57520f734cb
mishabogdanov5 <88720227+mishabogdanov5@users.noreply.github.com>
2024-11-26 13:35:09 +0300

    Migration to WPF Class Lib

--- CKL/RelationItem.cs
+++ CKL/RelationItem.cs
@@ -4,30 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CKLLib
+namespace CKLK
 {
-    public class RelationItem 
+    public class RelationItem
     {
-        public string[] Items { get; internal set; }
-        public DateTime[] StartTimes { get; internal set; }
-        public DateTime[] EndTimes { get; internal set; }
 
-        public RelationItem(string[] items, DateTime[] startTimes, DateTime[] endTimes) 
-        {
-            Items = items;
-            StartTimes = startTimes;
-            EndTimes = endTimes;
-        }
-
-        public override bool Equals(object? obj)
-        {
-            if (obj is not RelationItem) return false;
-            if (obj == null) return false;
-            
-            RelationItem item = obj as RelationItem;
-
-            return (item.Items.SequenceEqual(Items) && item.StartTimes.SequenceEqual(StartTimes) &&
-                item.EndTimes.SequenceEqual(EndTimes));
-        }
     }
 }
