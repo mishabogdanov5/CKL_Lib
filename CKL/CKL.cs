c6934ee385dccfde5bc316d37b0312e2e338222b
mishabogdanov5 <88720227+mishabogdanov5@users.noreply.github.com>
2024-11-27 12:54:27 +0300

    Time Transform Operation

--- CKL/CKL.cs
+++ CKL/CKL.cs
@@ -1,8 +1,45 @@
 
+using System.Runtime.InteropServices.ObjectiveC;
+
 namespace CKL
 {
     public class CKL
     {
+        public string Name { get; set; }
+        public DateTime StartTime { get; private set; }
+        public DateTime EndTime { get; private set; }
+        public HashSet<object> A { get; private set; }
+        public HashSet<object>? B { get; private set; }
+        public HashSet<RelationItem> Relation { get; private set; }
+
+        public CKL() 
+        {
+            Name = string.Empty;
+            StartTime = DateTime.MinValue;
+            EndTime = DateTime.MaxValue;
+            A = new HashSet<object>();
+            B = new HashSet<object>();
+            RelationItem = new HashSet<RelationItem>();
+        }
+        
+        public CKL(string name, DateTime startTime, DateTime endTime, HashSet<object> a, HashSet<RelationItem> relation) 
+        {
+            Name = string.Empty;
+            StartTime = startTime;
+            EndTime = endTime;
+            A = a;
+            Relation = relation;
+        }
+
+        public CKL(string name, DateTime startTime, DateTime endTime, HashSet<object> a, HashSet<object>? b, HashSet<RelationItem> relation)
+        {
+            Name = string.Empty;
+            StartTime = startTime;
+            EndTime = endTime;
+            A = a;
+            B = b;
+            Relation = relation;
+        }
     }
 
 }
--- CKL/RelationItem.cs
+++ CKL/RelationItem.cs
@@ -8,6 +8,22 @@ namespace CKL
 {
     public class RelationItem
     {
+        public object[] Value { get; private set; }
+        public DateTime[] StartTimes { get; private set; }
+        public DateTime[] EndTimes { get; private set; }
 
+        public RelationItem() 
+        {
+            Value = new object[2];
+            StartTimes = new DateTime[0];
+            EndTimes = new DateTime[0];
+        }
+
+        public RelationItem(object[] value, DateTime[] startTimes, DateTime[] endTimes) 
+        {
+            Value = value;
+            StartTimes = startTimes;
+            EndTimes = endTimes;
+        }
     }
 }
