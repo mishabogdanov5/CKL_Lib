672c23349d6da2f457fe4004a60acea2566b5150
mishabogdanov5 <88720227+mishabogdanov5@users.noreply.github.com>
2025-01-03 05:46:47 +0300

    ~add/ Ckl union operation

--- CKLLib/obj/Debug/net8.0/CKLLib.AssemblyInfo.cs
+++ CKLLib/obj/Debug/net8.0/CKLLib.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CKLLib")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b6423d9b60a0536f226ba9779768728a23d5910")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a60959194ace9043ee3454b78e705dae62d7fda3")]
 [assembly: System.Reflection.AssemblyProductAttribute("CKLLib")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CKLLib")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
